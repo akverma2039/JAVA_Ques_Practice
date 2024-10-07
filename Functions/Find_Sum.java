0917b2225277fde8cd5b1d52caee9070186a761a
Ankit Kumar Verma <akverma2039@gmail.com>
2024-10-07 19:22:19 +0530

    FindSum

---
+++ Functions/Find_Sum.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+public class Find_Sum {
+    
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+
+        // System.out.println("Enter First number: ");
+        // int a= sc.nextInt();
+        // System.out.println("Enter Second number: ");
+        // int b = sc.nextInt();
+
+        findSum(452,20);
+
+    }
+
+    static void findSum(int a, int b){
+
+        // Scanner sc = new Scanner(System.in);
+
+        // System.out.println("Enter First number: ");
+        // int a= sc.nextInt();
+        // System.out.println("Enter Second number: ");
+        // int b = sc.nextInt();
+
+        int sum = a + b;
+        System.out.println(" Your Sum is: "+sum);
+
+    }
+}
